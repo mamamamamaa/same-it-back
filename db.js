a4c0919b6c3cbb0cd5cb0a10dae707cf7488eb62
mamamamamaa <maks.denishchuk@gmail.com>
2023-03-19 22:27:36 +0200

    confugure db

--- db.js
+++ db.js
@@ -10,29 +10,10 @@ const pool = new Pool({
   port: POSTGRES_PORT,
 });
 
-pool.query(
-  "CREATE SCHEMA IF NOT EXISTS app;\n" +
-    "\n" +
-    "CREATE TYPE IF NOT EXISTS app.userRoles as ENUM ('admin', 'employee');\n" +
-    "CREATE TYPE IF NOT EXISTS app.stateType as ENUM ('male', 'female');\n" +
-    "\n" +
-    "CREATE TABLE IF NOT EXISTS app.users (\n" +
-    "    id SERIAL PRIMARY KEY,\n" +
-    "    username VARCHAR(50) NOT NULL DEFAULT '',\n" +
-    "    email VARCHAR(250) UNIQUE NOT NULL,\n" +
-    "    role app.userRoles,\n" +
-    "    dateCreate TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),\n" +
-    "    profileId INTEGER UNIQUE REFERENCES app.profiles(id) ON DELETE CASCADE\n" +
-    ");\n" +
-    "\n" +
-    "CREATE TABLE IF NOT EXISTS app.profiles (\n" +
-    "    id SERIAL PRIMARY KEY,\n" +
-    "    firstName VARCHAR(50) NOT NULL DEFAULT '',\n" +
-    "    secondName VARCHAR(50) NOT NULL DEFAULT '',\n" +
-    "    state app.stateType\n" +
-    ");\n" +
-    "\n" +
-    "\n"
-);
+pool.query("CREATE SCHEMA IF NOT EXISTS app;");
+pool.query("DROP TYPE IF EXISTS app.userRoles;");
+pool.query("CREATE TYPE app.userRoles as ENUM ('admin', 'employee');");
+pool.query("DROP TYPE IF EXISTS app.stateType;");
+pool.query("CREATE TYPE app.stateType as ENUM ('male', 'female');");
 
 module.exports = pool;
