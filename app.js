1cc2311fd7b1f41c201068fc9591e050a7a174db
mamamamamaa <maks.denishchuk@gmail.com>
2023-03-20 14:03:54 +0200

    add prisma migration

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 require("dotenv").config();
 
-const express = require("express");
 const cors = require("cors");
-const db = require("./db");
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
+
+async function main() {
+  const newUser = await prisma.user.create({
+    data: {
+      username: "john_doe",
+      email: "john_doe@example.com",
+      role: "employee",
+    },
+  });
+  console.log(newUser);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
 
 module.exports = app;
--- db.js
+++ db.js
@@ -3,17 +3,11 @@ const { Pool } = require("pg");
 const { POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_PORT } =
   process.env;
 
-const pool = new Pool({
-  user: POSTGRES_USER,
-  database: POSTGRES_DB,
-  password: POSTGRES_PASSWORD,
-  port: POSTGRES_PORT,
-});
+// const pool = new Pool({
+//   user: POSTGRES_USER,
+//   database: POSTGRES_DB,
+//   password: POSTGRES_PASSWORD,
+//   port: POSTGRES_PORT,
+// });
 
-pool.query("CREATE SCHEMA IF NOT EXISTS app;");
-pool.query("DROP TYPE IF EXISTS app.userRoles;");
-pool.query("CREATE TYPE app.userRoles as ENUM ('admin', 'employee');");
-pool.query("DROP TYPE IF EXISTS app.stateType;");
-pool.query("CREATE TYPE app.stateType as ENUM ('male', 'female');");
-
-module.exports = pool;
+// module.exports = pool;
